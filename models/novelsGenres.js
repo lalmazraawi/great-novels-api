dfe4cbce3009f3b56ffe92348bc881e071b9037f
Lailah Amazraawi <lailahamazraawi@Lailahs-MacBook-Pro.local>
2022-06-20 12:08:34 -0400

    re-completed api

--- models/novelsGenres.js
+++ models/novelsGenres.js
@@ -1,6 +1,5 @@
 const NovelsGenres = (connection, Sequelize, Novels, Genres) => {
     return connection.define('novelsGenres', {
-      id: { type: Sequelize.INTEGER, primaryKey: true },
       novelId: { type: Sequelize.INTEGER, references: { model: Novels, key: 'id' } },
       genreId: { type: Sequelize.INTEGER, references: { model: Genres, key: 'id' } },
       createdAt: { type: Sequelize.DATE },
